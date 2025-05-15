1fa7a0fba5b3ba4781afc533304df202ce1ceb9f
macrotek <ganuda1234@gmail.com>
2025-05-15 18:09:14 +0530

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CofeeShopWebAPI.WebApiApplication" Language="C#" %>
---
+++ Models/SubMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CofeeShopWebAPI.Models
+{
+    public class SubMenu
+    {
+        public int SubMenuId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Images { get; set; }
+        public ICollection<SubMenu> SubMenus { get; set; }
+    }
+}
